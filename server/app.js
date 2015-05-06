0fe80d9e74b220371157e92a9b633d8262d0e960
JeanneEricksonCooley <cool0020@gmail.com>
2015-05-05 19:52:32 -0500

    first commit

---
+++ server/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+var index = require('./routes/index');
+app.use("/", index);
+
+var server = app.listen(3000, function() {
+    var port = server.address().port;
+    console.log("Listening on port: ", port);
+});
+
+module.exports = app;
